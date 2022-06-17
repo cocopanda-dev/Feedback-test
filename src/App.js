af5a21a1c5b5b2b1e7063d15e0a36a9cf27aeff2
Crppckyz <fotdw1991@hotmail.com>
2022-06-16 22:59:06 -0400

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // npm i uuid (unique ID generator) 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Route, Router, Routes} from 'react-router-dom'
 import Header from './components/Header.jsx';
 import FeedbackList from './components/FeedbackList.jsx';
 import FeedbackStats from './components/FeedbackStats.jsx';
@@ -13,25 +13,27 @@ function App() {
 
   return (
     <FeedbackProvider>
-      <Header />
-      <div className='container'>
-        <Routes> 
-          <Route path='/' element={
-            <>
-              <FeedbackForm />
-              <FeedbackStats />
-              <FeedbackList />
-              <AboutIconLink />
-              
-            </>
-          }
-          ></Route>
-          
-          <Route path='/about' element={<AboutPage />} />
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes> 
+            <Route path='/' element={
+              <>
+                <FeedbackForm />
+                <FeedbackStats />
+                <FeedbackList />
+                <AboutIconLink />
+                
+              </>
+            }
+            ></Route>
+            
+            <Route path='/about' element={<AboutPage />} />
 
-          
-        </Routes>
-      </div>
+            
+          </Routes>
+        </div>
+      </Router>
     </FeedbackProvider>
   );
 }
